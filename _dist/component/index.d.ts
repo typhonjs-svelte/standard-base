2feb33e5f71680e42e8e986355c89a93280dd9b8
typhonrt <support@typhonjs.io>
2024-09-28 00:10:37 -0700

    Update dist.

--- _dist/component/index.d.ts
+++ _dist/component/index.d.ts
@@ -2536,13 +2536,13 @@ declare namespace TjsPositionControlLayer {
   /** Props type alias for {@link TjsPositionControlLayer | associated component}. */
   export type Props = {
     components?: any;
-    enabled?: boolean;
-    boundingRect?: any;
-    validate?: boolean;
     /**
      * @type {ControlsStore}
      */
     controls?: ControlsStore;
+    enabled?: boolean;
+    boundingRect?: any;
+    validate?: boolean;
   };
   /** Events type alias for {@link TjsPositionControlLayer | associated component}. */
   export type Events = { [evt: string]: CustomEvent<any> };
@@ -2661,6 +2661,93 @@ declare namespace TjsSideSlideLayer {
   export type Events = { [evt: string]: CustomEvent<any> };
   /** Slots type alias for {@link TjsSideSlideLayer | associated component}. */
   export type Slots = {};
+}
+
+/**
+ * Provides an initial implementation to display image or video content from a given file path.
+ *
+ * You can either set the `filepath` prop or use {@link TJSFileSlotButton} and embed TJSMediaContent as a child.
+ * A `filepath` context / store will be examined if it exists to obtain a file path to load.
+ *
+ * The following CSS variables control the associated styles with the default values:
+ * ```
+ * --tjs-media-content-background - transparent
+ * --tjs-media-content-border - none
+ * --tjs-media-content-border-radius - 0
+ * --tjs-media-content-diameter - When defined used for height / width.
+ * --tjs-media-content-height - 100px
+ * --tjs-media-content-object-fit - contain
+ * --tjs-media-content-width - 100px
+ * ```
+ */
+declare class TjsMediaContent extends SvelteComponent<
+  TjsMediaContent.Props,
+  TjsMediaContent.Events,
+  TjsMediaContent.Slots
+> {}
+
+/** Event / Prop / Slot type aliases for {@link TjsMediaContent | associated component}. */
+declare namespace TjsMediaContent {
+  /** Props type alias for {@link TjsMediaContent | associated component}. */
+  export type Props = {
+    media?: any;
+    /**
+     * A title for the media element.
+     *
+     * @type {string}
+     */
+    title?: string;
+    /**
+     * Prop for filepath media content.
+     *
+     * @type {string}
+     */
+    filepath?: string;
+    /**
+     * Alternate image text.
+     *
+     * @type {string}
+     */
+    imgAlt?: string;
+    /**
+     * Automatically start video playback; default: true
+     *
+     * @type {boolean}
+     */
+    videoAutoplay?: boolean;
+    /**
+     * Automatically loop video; default: true
+     *
+     * @type {boolean}
+     */
+    videoLoop?: boolean;
+    /**
+     * Mute video playback audio; default: true
+     *
+     * @type {boolean}
+     */
+    videoMuted?: boolean;
+    /**
+     * Play video on pointer hover.
+     *
+     * @type {boolean}
+     */
+    videoPlayOnHover?: boolean;
+    /**
+     * Video playback rate - clamped internally between 0 - 2.
+     *
+     * @type {number}
+     */
+    videoPlaybackRate?: number;
+    /**
+     * TODO: provide type and make this generic. Also figure out a non-Foundry default embedded SVG.
+     */
+    defaultMedia?: { elementType: string; filepath: string; valid: boolean };
+  };
+  /** Events type alias for {@link TjsMediaContent | associated component}. */
+  export type Events = { [evt: string]: CustomEvent<any> };
+  /** Slots type alias for {@link TjsMediaContent | associated component}. */
+  export type Slots = { 'video-fallback': {} };
 }
 
 /**
@@ -3046,6 +3133,7 @@ export {
   TjsInputCheckbox as TJSInputCheckbox,
   TjsInputNumber as TJSInputNumber,
   TjsInputText as TJSInputText,
+  TjsMediaContent as TJSMediaContent,
   TjsMenu as TJSMenu,
   type TJSMenuData,
   type TJSMenuItemData,

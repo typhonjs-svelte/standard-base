e1e4ba7fe4ad2f0ef1be0571b0455e1e7ebdd49b
typhonrt <support@typhonjs.io>
2024-11-16 21:54:11 -0800

    Add `TJSInputRangeNumber` component.

--- src/component/form/input/index.js
+++ src/component/form/input/index.js
@@ -2,5 +2,5 @@ export { default as TJSInput }            from './TJSInput.svelte';
 export { default as TJSInputCheckbox }    from './TJSInputCheckbox.svelte';
 export { default as TJSInputNumber }      from './TJSInputNumber.svelte';
 export { default as TJSInputRange }       from './TJSInputRange.svelte';
-export { default as TJSInputRangeNumber } from './TJSInputRange.svelte';
+export { default as TJSInputRangeNumber } from './TJSInputRangeNumber.svelte';
 export { default as TJSInputText }        from './TJSInputText.svelte';

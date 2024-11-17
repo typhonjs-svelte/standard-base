ab2aa2ee0e276e1aa744e6371a73b342d12dee57
typhonrt <support@typhonjs.io>
2024-11-16 20:53:13 -0800

    Add `TJSInputRangeNumber` component.

---
+++ src/component/form/input/TJSInputRangeNumber.svelte
@@ -0,0 +1,74 @@
+<script>
+   /**
+    * Provides a combined {@link TJSInputRange} / {@link TJSInputNumber} component with a single slotted label wrapper.
+    * This is a convenience component enabling easy hook up of a range + number input from the same data source.
+    *
+    * There is no explicit layout defined. The `input` data is forwarded on to both range / number components with
+    * the exception that `readonly` only applies to the number input.
+    *
+    * Note: The `efx` animation action applies to both the range / number inputs.
+    *
+    * @componentDocumentation
+    */
+   import { writable }                 from 'svelte/store';
+
+   import { isMinimalWritableStore }   from '#runtime/svelte/store/util';
+   import { isObject }                 from '#runtime/util/object';
+
+   import {
+      TJSSlotLabel,
+      TJSSlotLabelUtil }               from '#standard/component/label';
+
+   import TJSInputNumber               from './TJSInputNumber.svelte';
+   import TJSInputRange                from './TJSInputRange.svelte';
+
+   export let input = void 0;
+
+   export let disabled = void 0;
+   export let label = void 0;
+   export let options = void 0;
+   export let max = void 0;
+   export let min = void 0;
+   export let readonly = void 0;
+   export let step = void 0;
+   export let store = void 0;
+   export let styles = void 0;
+
+   export let efx = void 0;
+
+   // ----------------------------------------------------------------------------------------------------------------
+
+   $: disabled = isObject(input) && typeof input.disabled === 'boolean' ? input.disabled :
+    typeof disabled === 'boolean' ? disabled : false;
+
+   $: label = isObject(input) && TJSSlotLabelUtil.isValid(input.label) ? input.label :
+    TJSSlotLabelUtil.isValid(label) ? label : void 0;
+
+   $: max = isObject(input) && typeof input.max === 'number' ? input.max :
+    typeof max === 'number' ? max : 100;
+
+   $: min = isObject(input) && typeof input.min === 'number' ? input.min :
+    typeof min === 'number' ? min : 0;
+
+   $: options = isObject(input) && isObject(input.options) ? input.options : isObject(options) ? options : {};
+
+   $: readonly = isObject(input) && typeof input.readonly === 'boolean' ? input.readonly :
+    typeof readonly === 'boolean' ? readonly : false;
+
+   $: step = isObject(input) && typeof input.step === 'number' ? input.step :
+    typeof step === 'number' ? step : 1;
+
+   $: store = isObject(input) && isMinimalWritableStore(input.store) ? input.store :
+    isMinimalWritableStore(store) ? store : writable(void 0);
+
+   $: styles = isObject(input) && isObject(input.styles) ? input.styles :
+    isObject(styles) ? styles : void 0;
+
+   $: efx = isObject(input) && typeof input.efx === 'function' ? input.efx :
+    typeof efx === 'function' ? efx : () => {};
+</script>
+
+<TJSSlotLabel {label} {disabled}>
+   <TJSInputRange label={false} {disabled} {efx} {max} {min} {options} {step} {store} {styles} />
+   <TJSInputNumber label={false} {disabled} {efx} {max} {min} {options} {readonly} {step} {store} {styles} />
+</TJSSlotLabel>
